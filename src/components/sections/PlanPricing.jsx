86f2b25d7d7c0716527cebf5d9332f9000d3e771
vyomPriyesh <vyompriyesh@gmail.com>
2026-08-22 21:51:54 +0530

    Fix package purchase status logic in PlanPricing

--- src/components/sections/PlanPricing.jsx
+++ src/components/sections/PlanPricing.jsx
@@ -85,7 +85,7 @@ const PlanPricing = () => {
         const expire_time = packages?.package_expire ? `Expired At ${unixDisplayDate(packages?.package_expire)}` : null
 
         return {
-            not_purchased: packages?.package_expire_status ? false : !packages,
+            not_purchased: packages?.package_expire_status ? true : !packages,
             expire_time: packages?.package_expire_status ? 'Renew Package' : packages ? expire_time : null
 
         }
